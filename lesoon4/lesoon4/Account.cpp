b17d90024d40b3a19b74602c60c4651fb2abcdc7
LeaLiv <leahl11730@gmail.com>
2025-03-25 21:45:42 +0200

    in the middle of making constractors

--- lesoon4/lesoon4/Account.cpp
+++ lesoon4/lesoon4/Account.cpp
@@ -1,6 +1,24 @@
 #include "Account.h"
+#include <iostream>
+//#include <string_view> 
+//#include <string>
+//using namespace std;
 
 Account::Account(int accountNumber, int code, int balance, string email)
 {
-	if(code<1000 || code >9999 || )
+	if(code<1000 || code >9999 )
+		std::cout << "Hello World!\n";
+}
+
+bool Account::isValidEmail(const string& email) {
+    size_t atPos = email.find('@');
+    if (atPos == string::npos || atPos == 0 || atPos == email.length() - 1) return false;
+
+    string localPart = email.substr(0, atPos);
+    string domain = email.substr(atPos + 1);
+
+    if (localPart.find(' ') != string::npos || domain.find(' ') != string::npos) return false;
+    if (!(domain.ends_with(".com") || domain.ends_with(".co.il"))) return false;
+
+    return true;
 }
--- lesoon4/lesoon4/Account.h
+++ lesoon4/lesoon4/Account.h
@@ -1,13 +1,16 @@
 #pragma once
 #include <string>
-
-
+using namespace std;
 
 class Account
 {
 	int accountNumber;
-	string code;
-	double balance;
-	char* email;
+	int code;
+	int balance;
+	string email;
+public:
+	Account():accountNumber(0),code(0),balance(0),email(""){}
+	Account(int accountNumber, int code, int balance, string email);
+	bool isValidEmail(const string& email);
 };
 
