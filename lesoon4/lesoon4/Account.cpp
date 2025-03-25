dfd14f5f50b8a6d981d1b6f92aa686e42a8b8b9f
LeaLiv <leahl11730@gmail.com>
2025-03-25 15:18:40 +0200

    initial commit

---
+++ lesoon4/lesoon4/Account.cpp
@@ -0,0 +1,6 @@
+#include "Account.h"
+
+Account::Account(int accountNumber, int code, int balance, string email)
+{
+	if(code<1000 || code >9999 || )
+}
---
+++ lesoon4/lesoon4/Account.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <string>
+
+
+
+class Account
+{
+	int accountNumber;
+	string code;
+	double balance;
+	char* email;
+};
+
